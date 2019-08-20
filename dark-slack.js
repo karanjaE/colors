17c47805cb71247de5ed7762afdf7e71fa8a542f
karanjaE <rranchow@gmail.com>
2019-08-20 18:02:30 +0300

    blah

---
+++ dark-slack.js
@@ -0,0 +1,13 @@
+// enable dark theme for slack
+document.addEventListener('DOMContentLoaded', function() {
+  $.ajax({
+    url: 'https://cdn.rawgit.com/laCour/slack-night-mode/master/css/raw/black.css',
+    success: function(css) {
+      let overrides = `
+      code { background-color: #4D5250; color: #85c5ff; } /* Change color: to whatever font color you want */
+      .c-mrkdwn__pre, .c-mrkdwn__quote { background: #535353 !important; background-color: #535353 !important; }
+      `
+      $("<style></style>").appendTo('head').html(css + overrides);
+    }
+  });
+ });
